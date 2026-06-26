01232a3a6344318b7ac469fb7da6a216bea87f89
chauhannishith <chauhannishith94@gmail.com>
2026-06-26 14:28:29 +0530

    Extract default theme into embedded template files

---
+++ templates.go
@@ -0,0 +1,32 @@
+package hermes
+
+import (
+	"embed"
+	"fmt"
+)
+
+//go:embed templates
+var staticFS embed.FS
+
+const (
+	htmlEmail      = "templates/%s.tpl.html"
+	plainTextEmail = "templates/%s.tpl.txt"
+)
+
+func getTemplate(name string) string {
+	htmlBytes, err := staticFS.ReadFile(name)
+	if err != nil {
+		panic(fmt.Sprintf("hermes: read template %s: %v", name, err))
+	}
+
+	return string(htmlBytes)
+}
+
+// GetDefaultStyles returns the parsed CSS styles for the default theme.
+func GetDefaultStyles() StylesDefinition {
+	cssBytes, err := staticFS.ReadFile("templates/default.css")
+	if err != nil {
+		panic(fmt.Sprintf("hermes: read default.css: %v", err))
+	}
+	return ParseStylesDefinition(string(cssBytes))
+}
